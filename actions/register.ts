63de4869cafacc134748a5b58f25dad58ae491f7
andikadwikid <andika0106dwiki@gmail.com>
2025-08-24 14:00:34 +0700

    register

--- actions/register.ts
+++ actions/register.ts
@@ -1,14 +1,36 @@
 "use server";
 
-import { RegisterSchema } from "@/schema";
+import {db} from "@/lib/db";
+import bcrypt from "bcryptjs";
+import {RegisterSchema} from "@/schema";
 import * as z from "zod";
+import {getUserByEmail} from "@/data/user";
 
 export const register = async (value: z.infer<typeof RegisterSchema>) => {
-  const validatedFields = RegisterSchema.safeParse(value);
+    const validatedFields = RegisterSchema.safeParse(value);
 
-  if (!validatedFields.success) {
-    return { error: "Invalid fields!" };
-  }
+    if (!validatedFields.success) {
+        return {error: "Invalid fields!"};
+    }
 
-  return { success: "Registration successful" };
+    const {name, email, password} = validatedFields.data
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+        return {error: "Email already in use!"};
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        }
+    })
+
+    // TODO: Send verification token email
+
+    return {success: "Registration successful"};
 };
